f5531e13df63ff739af4933b4dcf7eaa0d79baa9
usha-12 <usha12ahirwar@gmail.com>
2023-02-08 13:27:06 +0530

    Added: User Need to enter a valid first name

--- src/main/java/com/brideglabz/junit/UserValidation.java
+++ src/main/java/com/brideglabz/junit/UserValidation.java
@@ -1,2 +1,10 @@
-package com.brideglabz.junit;public class UserValidation {
+package com.brideglabz.junit;
+
+import java.util.regex.Pattern;
+
+public class UserValidation {
+    public static final String VALIDATE_NAME="^[A-z]+[a-z]{2,}$";
+    public boolean checkFirstName(String firstName) {
+        return (Pattern.matches(VALIDATE_NAME, firstName));
+    }
 }
--- src/test/java/com/brideglabz/junit/RegistrationTest.java
+++ src/test/java/com/brideglabz/junit/RegistrationTest.java
@@ -1,2 +1,22 @@
-package com.brideglabz.junit;public class RegistrationTest {
+package com.brideglabz.junit;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+public class RegistrationTest {
+    @BeforeAll
+    static void Msg(){
+        System.out.println("Welcome to User Registration Problem");
+    }
+    @AfterEach
+    public void afterMsg(){
+        System.out.println("Passed");
+    }
+    @Test
+    void given_first_name_should_returnTrue() {
+        UserValidation validation = new UserValidation();
+        Boolean result = validation.checkFirstName("Aditya");
+        Assertions.assertEquals(result, true);
+    }
 }
