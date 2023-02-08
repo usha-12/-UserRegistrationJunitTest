62f389d2cbb92db8452a1d6c9d171edd078a93fd
usha-12 <usha12ahirwar@gmail.com>
2023-02-08 13:44:24 +0530

    Added: User enter should have at least 1 upper case

--- src/test/java/com/brideglabz/junit/RegistrationTest.java
+++ src/test/java/com/brideglabz/junit/RegistrationTest.java
@@ -43,10 +43,14 @@ public class RegistrationTest {
         Boolean result4 = validation.password("ussaahir");
         Assertions.assertEquals(result4, true);
     }
+    @Test
+    void given_password1_should_returnTrue() {
+        UserValidation validation = new UserValidation();
+        Boolean result5 = validation.password2("Ussaahir");
+        Assertions.assertEquals(result5, true);
+    }
 }
-/*As a User need to
-follow pre-defined
-Password rules.
-Rule1
-– minimum 8
-Characters - NOTE – All rules must be passed*/
+/*Rule2
+– Should
+have at least 1
+Upper Case - NOTE – All rules must be passed*/
